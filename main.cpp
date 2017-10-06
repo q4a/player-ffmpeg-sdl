706200b0d9f2ec4b692915e3b2f6874e10ec0202
Rangel Reale <rangelspam@gmail.com>
2017-10-06 16:52:14 -0300

    * Add problem video

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@ int main(int argc, char** argv) {
 			//"M:\\transfer\\use\\anime\\Clannad - 01\\[SS-Eclipse]_Clannad_-_02_(1024x576_h264)_[F952E06B].mkv"
 			//"M:\\transfer\\use\\documentary_temp\\American Propaganda Films - Capitalism (1948).avi"
 			//"M:\\transfer\\use\\documentary_temp\\BBC - Isaac Newton - The Dark Heretic.svcd.mpg"
-			"M:\\transfer\\use\\documentary_temp\\Discovery - Robosapiens.mpg"
+			//"M:\\transfer\\use\\documentary_temp\\Discovery - Robosapiens.mpg"
+			"M:\\transfer\\use\\documentary_temp\\mysteries.of.egypt.imax.1998.dvdrip.divx-qix.avi"
 			;
 
 		Player play{ filename };
